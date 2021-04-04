edd1183a63d6f0a4085001fb86f7d93f727d3dfc
egi <daegi01@gmail.com>
2021-04-04 23:22:10 +0200

    Bugfix for SceneManager.GetActiveScene() => "Startup" issue, for better compatibility with other mods.

--- src/SkipIntro/Mod.cs
+++ src/SkipIntro/Mod.cs
@@ -25,10 +25,10 @@ namespace SkipIntro
             }
 
             _harmony = HarmonyInstance.Create(_harmonyId);
-            TranspileIntroScreensToEmptyStrings(_harmony);
+            TranspileAwayTheIntroScreens(_harmony);
         }
 
-        private void TranspileIntroScreensToEmptyStrings(HarmonyInstance harmony)
+        private void TranspileAwayTheIntroScreens(HarmonyInstance harmony)
         {
             var loadIntroCoroutineOriginal = typeof(LoadingManager)
                 .GetNestedTypes(BindingFlags.NonPublic)
@@ -54,31 +54,57 @@ namespace SkipIntro
 
         private static IEnumerable<CodeInstruction> LoadIntroCoroutineTranspiler(IEnumerable<CodeInstruction> codeInstructions)
         {
-            foreach (var codeInstruction in codeInstructions)
+            var menuSceneField = typeof(LoadingManager)
+                .GetNestedTypes(BindingFlags.NonPublic)
+                .Single(x => x.FullName == "LoadingManager+<LoadIntroCoroutine>c__Iterator0")
+                .GetField("<menuScene>__0", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var codes = codeInstructions.ToList();
+
+            for (int i = 0; i < codes.Count; i++)
             {
-                if (codeInstruction.opcode == OpCodes.Ldstr
+                var code = codes[i];
+
+                if (code.opcode == OpCodes.Ldstr
                     && (
-                        codeInstruction.operand as string == "IntroScreen"
-                        || codeInstruction.operand as string == "IntroScreen2"
+                        code.operand as string == "IntroScreen"
+                        || code.operand as string == "IntroScreen2"
                     )
                 )
                 {
-                    codeInstruction.operand = string.Empty;
+                    code.operand = string.Empty;
                 }
 
-                if (codeInstruction.opcode == OpCodes.Ldc_R4 
+                if (code.opcode == OpCodes.Ldc_R4
                     && (
-                        codeInstruction.operand as float? == 4f //wait time of IntroScreen and FadeImage
-                        || codeInstruction.operand as float? == 1f //wait time of IntroScreen2
-                        || codeInstruction.operand as float? == 20f //wait time for IsDLCStateReady and LegalDocumentsReady
+                        code.operand as float? == 4f //wait time of IntroScreen and FadeImage
+                        || code.operand as float? == 1f //wait time of IntroScreen2
+                        || code.operand as float? == 20f //wait time for IsDLCStateReady and LegalDocumentsReady
                     )
                 )
                 {
-                    codeInstruction.operand = 0f;
+                    code.operand = 0f;
                 }
 
-                yield return codeInstruction;
+                /*
+                Replace
+                    yield return SceneManager.LoadSceneAsync(menuScene, LoadSceneMode.Additive);
+                with
+                    yield return SceneManager.LoadSceneAsync(menuScene, LoadSceneMode.Single);
+                Ensures that SceneManager.GetActiveScene() reports "MainMenu" instead of "Startup".
+                Default is "IntroScreen" due to 'LoadSceneMode.Additive' but mods that use GetActiveScene() should check
+                for "IntroScreen" || "MainMenu" anyway since its "MainMenu" if you go from game back to main menu.
+                */
+                if (code.opcode == OpCodes.Ldarg_0
+                    && codes[i + 1].opcode == OpCodes.Ldarg_0
+                    && codes[i + 2].opcode == OpCodes.Ldfld && codes[i + 2].operand == menuSceneField
+                    && codes[i + 3].opcode == OpCodes.Ldc_I4_1)
+                {
+                    codes[i + 3].opcode = OpCodes.Ldc_I4_0;
+                }
             }
+
+            return codes;
         }
     }
 }
